257a2d3addd9e251e35593689963b39a401137c0
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-06-28 15:09:10 +0530

    refactor

--- index.sql
+++ index.sql
@@ -1,10 +1,26 @@
 
+
+
+
+
+
 show index 
 from table_name;
 
 
+
+create fulltext index index_name
+on table_name(column_name);
+
+select column_name
+from table_name
+where match(column_name) against(value);
+
+
+
 create index index_name
 on table_name(column_name);
+
 
 
 drop index index_name
